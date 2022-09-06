ba8bfdf5845e0da7ecda8b22a3f425e79728caa0
Sagar Patil <sagarreddypatil@gmail.com>
2022-09-06 12:04:46 -0400

    fancy

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, Response, render_template
 from main import do_the_thing
 
 app = Flask(__name__)
 
 
-@app.route("/gradescope")
+@app.route("/")
+def index():
+    # use tempelate index.html
+    return render_template("index.html")
+
+
+@app.route("/gradescope.ics")
 def gradescope_ics():
     args = request.args
     email = args.get("email")
     pwd = args.get("pwd")
 
-    response = make_response("".join(do_the_thing(email, pwd)))
-    response.headers["Content-Disposition"] = "attachment; filename=gradescope.ics"
-    return response
+    sem = args.get("sem")
+    semSeason = args.get("semSeason")
+    semYear = args.get("semYear")
+
+    if sem is None and semSeason is not None and semYear is not None:
+        sem = f"{semSeason} {semYear}"
+
+    ical = "".join(
+        do_the_thing(
+            email,
+            pwd,
+            sem,
+        )
+    )
+
+    return Response(ical, mimetype="text/calendar")
